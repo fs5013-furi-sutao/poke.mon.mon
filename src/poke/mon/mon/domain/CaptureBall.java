d255ec1ff0b8cd8db2750b71b94bf442487b5c9d
fs5013-furi-sutao <fs5013.furi.sutao@gmail.com>
2020-08-10 22:13:13 +0900

    ポイント計算の修正

--- src/poke/mon/mon/domain/CaptureBall.java
+++ src/poke/mon/mon/domain/CaptureBall.java
@@ -1,35 +1,73 @@
 package poke.mon.mon.domain;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.util.Random;
 import poke.mon.mon.config.BallType;
 import poke.mon.mon.util.Console;
 
 public class CaptureBall {
 
-    private int      quantity;
+    private int quantity;
     private BallType type;
-    private Console  con;
+    private Console con;
 
     CaptureBall(BallType type) {
-        this.type     = type;
+        this.type = type;
         this.quantity = type.getQuantity();
-        this.con      = new Console();
+        this.con = new Console();
     }
 
     public String infomationAgainstMonster(Monster mon) {
 
         String infoFormat = "%sを使う(残り%d個。捕獲成功率:%d%%)";
-        String info       = String
-                .format(
-                        infoFormat,
-                        this.type.getName(),
-                        quantity,
-                        this.getCaptureSuccessRate(mon));
+        String info = String.format(infoFormat, this.type.getName(), quantity,
+                this.getCaptureSuccessRate(mon));
         return info;
     }
 
+    /**
+     * すべてのボールの確率を算出し、 もし、最大の確率が 100% を超えていたら 最大の確率のものを 100% とし、残りは相対的な確立に修正する
+     * 
+     * @param mon
+     * @return
+     */
     private int getCaptureSuccessRate(Monster mon) {
-        return mon.getCaptureRate() + this.type.getCorrectRate();
+        int maxRate = this.getMaxCaptureRateInAllBalls(mon);
+        System.out.println("maxRate=" + maxRate);
+        int mineRate = mon.getCaptureRate() + this.type.getCorrectRate();
+        System.out.println("mineRate=" + maxRate);
+        if (maxRate >= 100) {
+            mineRate = this.getCorrectionValue(maxRate, mineRate);
+            System.out.println("come");
+        }
+        return mineRate;
+    }
+
+    private int getCorrectionValue(int maxRate, int mineRate) {
+        BigDecimal rigidMaxRate = BigDecimal.valueOf(maxRate);
+        BigDecimal rigidRange = BigDecimal.valueOf(100);
+
+        BigDecimal correctionRate = rigidRange.divide(rigidMaxRate
+                , 0/* ← scale */
+                , RoundingMode.HALF_UP /* ← 四捨五入 */
+            );
+
+        BigDecimal rigidMineRate = BigDecimal.valueOf(mineRate);
+        BigDecimal rigidAnswer = rigidMineRate.multiply(correctionRate);
+
+        return rigidAnswer.intValue();
+    }
+
+    private int getMaxCaptureRateInAllBalls(Monster mon) {
+        int tempRate = 0;
+        int maxRate = 0;
+        for (BallType type : BallType.values()) {
+            tempRate = mon.getCaptureRate() + type.getCorrectRate();
+            if (tempRate > maxRate)
+                maxRate = tempRate;
+        }
+        return maxRate;
     }
 
     public BallType getType() {
@@ -72,7 +110,7 @@ public class CaptureBall {
     }
 
     private int generateChanceNum() {
-        Random    rand       = new Random();
+        Random rand = new Random();
         final int RATE_RANGE = 100;
         return rand.nextInt(RATE_RANGE);
     }
